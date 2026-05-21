eaf9716a2953aeeb04271a9306313c79baeaa68c
ayssiareads-astro <ayssia.mason@gmail.com>
2026-05-21 11:37:51 -0400

    Add cache control headers to response

--- api/birth-chart.js
+++ api/birth-chart.js
@@ -385,6 +385,9 @@ const houseLabel = correctHouse
       }
     }
 
+    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Surrogate-Control", "no-store");
     return res.status(200).json({
       name: name || "Your",
       city,
